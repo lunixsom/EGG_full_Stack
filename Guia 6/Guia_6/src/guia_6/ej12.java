44e9d918d4635467e28f706d30be3f0ae4fa6b58
Emanuel Some <em7som@gmail.com>
2023-03-01 10:59:09 -0300

    Ejercicio 12 g6 finalizado - esMultiplo

--- Guia 6/Guia_6/src/guia_6/ej12.java
+++ Guia 6/Guia_6/src/guia_6/ej12.java
@@ -13,7 +13,24 @@ import java.util.Scanner;
  */
 public class ej12 {
      public static void main(String[] args) {
-            
+         
+         int num1, num2;
+         
+         Scanner leer = new Scanner(System.in);
+         
+         System.out.println("Ingrese los dos numeros: ");
+         
+         num1 = leer.nextInt();
+         num2 = leer.nextInt();
+         
+         esMultiplo(num1, num2);
      }
-
+     
+     public static void esMultiplo(int num1, int num2){
+         if ((num1 % num2) == 0){
+             System.out.println("El primer numero es multiplo del segundo");
+        } else {
+             System.out.println("No son multiplos");
+          }
+     }
 }

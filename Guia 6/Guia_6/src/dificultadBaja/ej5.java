0bdaa826bd0d3f6e597039aaa71d600307edf715
Ema notebookMarce <em7som@gmail.com>
2023-03-02 11:14:32 -0300

    ej5 sin resolver

---
+++ Guia 6/Guia_6/src/dificultadBaja/ej5.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dificultadBaja;
+
+import java.util.Scanner;
+/**
+ *
+ * @author esome
+ */
+public class ej5 {
+    public static void main(String[] args){
+        double num;
+    
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        num = leer.nextDouble();
+        
+        mostrarDoble(num);
+        mostrarTriple(num);
+        mostrarRaizCuadrada(num);
+    }
+    
+    public static void mostrarDoble(double num){
+        System.out.println("El doble del numero ingresado es: " + num*num);
+    }
+    
+    public static void mostrarTriple(double num){
+        System.out.println("El triple del numero ingresado es: " + num*num*num);
+    }
+        
+    public static void mostrarRaizCuadrada(double num){
+        System.out.println("La raiz cuadrada del numero ingresado es: " + sqrt(double num));
+    }
+}

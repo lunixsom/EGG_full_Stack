c38623494f813c9defa8332c260c799092e9850f
Emanuel Some <em7som@gmail.com>
2023-03-09 20:01:55 -0300

    Dificultad Media - ej13 - RESUELTO

--- Guia 6/Guia_6/src/dificultadMedia/ej13.java
+++ Guia 6/Guia_6/src/dificultadMedia/ej13.java
@@ -15,24 +15,31 @@ public class ej13 {
         System.out.print("Ingrese cantidad de elementos: ");
         cant = leer.nextInt();
         
-        for (int i = 0; i < cant; i++) {
-                if(i == 0 || i == (cant-1)){
+    // Genero solo la primer linea de * :    
+        for (int i = 0; i < 1; i++) {                
             for (int j = 0; j < cant; j++) {
-                    System.out.print("*");                
-                    } 
-                } else{
-           for (int x = 0; x < cant-2 ; x++) {
+                System.out.print("*");                
+            } 
+            System.out.println("");
+        }
+        
+    // Genero las filas con los espacios vacios:    
+        for (int i = 0; i < cant - 2; i++) {
             System.out.print("*");
-            for (int j = 0; j < cant-2; j++) {
+            for (int j = 0; j<cant-2; j++) {
                 System.out.print(" ");
             }
             System.out.println("*");
         }
-                }   
-            System.out.println("");
-            }
+        
+    // Genero la ultima linea de * :    
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < cant; j++) {
+                System.out.print("*");                
+            } 
+        }      
+    }
+}       
         
 
-        }
-    }
 

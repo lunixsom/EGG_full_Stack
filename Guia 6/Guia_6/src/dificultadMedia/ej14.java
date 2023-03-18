f8c7d8cb9b97259e830c0ec1baf303361a000c01
unknown <Administrador@A1347.intraudesa.edu.ar>
2023-03-18 16:11:08 -0300

    Dificultad Media - Ej14 - RESUELTO

--- Guia 6/Guia_6/src/dificultadMedia/ej14.java
+++ Guia 6/Guia_6/src/dificultadMedia/ej14.java
@@ -9,22 +9,52 @@ import java.util.Scanner;
 
 public class ej14 {
     public static void main(String[] args){
-        double euros;
-        String moneda;
-        Scanner leer = new Scanner(System.in);
         
-        System.out.println("Ingrese cantidad en Euros: ");
-        euros = leer.nextDouble();
-        System.out.println("Ingrese a que moneda realizar conversion: ");
-        moneda = leer.nextLine();
+        double euros = solicitarEuros();
+        String moneda = solicitarMoneda();
         
         convertirMonedayMostrar(euros,moneda);
     }
     
     public static void convertirMonedayMostrar(double euros,String moneda){
-        double vectorMonedas[] = {12.3,1.22,129.123};
+        double vectorMonedas[] = {1.28611,129.852,0.86};
         
-        System.out.println("vec: " + vectorMonedas[1]);
-        System.out.println("conversion: " + (euros*vectorMonedas[0]));
+        switch (moneda){
+        case "dolar":
+            System.out.println("conversion: " + (euros*vectorMonedas[0]));
+            break;
+        case "yen":
+            System.out.println("conversion: " + (euros*vectorMonedas[1]));
+            break;
+        case "libra":
+            System.out.println("conversion: " + (euros*vectorMonedas[2]));
+            break;
+        default:
+            System.out.println("La moneda seleccionada es incorrecta");
+            solicitarEuros();
+            solicitarMoneda();
+        }
+    }
+    
+    public static double solicitarEuros(){
+        double euros;
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("Ingrese cantidad en Euros:");
+        euros = leer.nextDouble();
+                
+        return euros;
+    }
+    
+        public static String solicitarMoneda(){
+        String moneda;
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("Ingrese moneda a convertir (Dolar | Yen | Libra) :");
+        moneda = leer.nextLine();
+                
+        return moneda;
     }
 }

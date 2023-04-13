50e0084cb5e445c3b431044bd79a646a168b874b
Emanuel Some <em7som@gmail.com>
2023-04-13 13:30:09 -0300

    PASAJE POR VALOR & REFERENCIA - video 9 terminado

--- Guia 7/MascotAPP/src/mascotapp/MascotAPP.java
+++ Guia 7/MascotAPP/src/mascotapp/MascotAPP.java
@@ -1,7 +1,6 @@
 
 package mascotapp;
 
-import java.util.Scanner;
 import mascotapp.entidades.Mascota;
 import mascotapp.servicios.ServicioMascota;
 
@@ -9,11 +8,20 @@ public class MascotAPP {
 
     public static void main(String[] args) {
        
+        int a = 10;
+        
         ServicioMascota sm = new ServicioMascota();
         
         Mascota m1 = sm.crearMascota();
         
+        Mascota mm = new Mascota();
+        
+        modificador(a, mm);
+        
         System.out.println(m1);
+        
+        System.out.println("a: " +a);
+        System.out.println("Mascota " +mm);
         
 //    public String nombre;
 //    public String apodo;
@@ -23,6 +31,12 @@ public class MascotAPP {
 //    public String raza;
 //    public int edad;
 //    public boolean cola;
+    }
+    
+    public static void modificador(int num, Mascota m){
+        
+        num = 100;
+        m.setApodo("Mascota Referencia");
     }
     
 }

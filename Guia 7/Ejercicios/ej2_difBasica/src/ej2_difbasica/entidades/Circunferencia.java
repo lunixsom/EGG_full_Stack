17f9024b6982e82bfb689645ff3a7c9c61b2d820
em7som_a1324 <em7som@gmail.com>
2023-04-18 17:55:14 -0300

    Ej2 - INCOMPLETO

---
+++ Guia 7/Ejercicios/ej2_difBasica/src/ej2_difbasica/entidades/Circunferencia.java
@@ -0,0 +1,27 @@
+
+package ej2_difbasica.entidades;
+
+public class Circunferencia {
+  
+    private float radio;
+    
+    // a)
+    
+    public Circunferencia(){}
+    
+    public Circunferencia(float radio){
+        this.radio = radio;
+    }
+
+    // b)
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+    
+    
+    
+}

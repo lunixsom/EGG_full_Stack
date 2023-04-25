8cf1406f32ef46709208dd9df595fd0c6d088bfb
em7som_a1324 <em7som@gmail.com>
2023-04-25 15:59:04 -0300

    EJ7 - RESUELTO

--- Guia 7/Ejercicios/ej7_difBasica/src/ej7_difbasica/Ej7_difBasica.java
+++ Guia 7/Ejercicios/ej7_difBasica/src/ej7_difbasica/Ej7_difBasica.java
@@ -15,16 +15,23 @@ public class Ej7_difBasica {
         //Persona p4 = sv.crearPersona();
         
         System.out.println("imc "+p1.calcularIMC());
-        if (p1.calcularIMC() == -1){
-            System.out.println("Su peso esta debajo del ideal");
-        } if (p1.calcularIMC() == 0){
-            System.out.println("Su peso es el ideal");
-        } if (p1.calcularIMC() == 1){
-            System.out.println("Tiene sobrepeso");
-        }
+
+        decimeMiEstadoPeso(p1);
         
         Persona arrayP[] = {p1, p2, p3};
+        
         porcentajesEdad(arrayP);
+        porcentajesPeso(arrayP);
+    }
+    
+    public static void decimeMiEstadoPeso(Persona unaPersona){
+        if (unaPersona.calcularIMC() == -1){
+            System.out.println("Su peso esta debajo del ideal");
+        } if (unaPersona.calcularIMC() == 0){
+            System.out.println("Su peso es el ideal");
+        } if (unaPersona.calcularIMC() == 1){
+            System.out.println("Tiene sobrepeso");
+        }
     }
     
     public static void porcentajesEdad(Persona[] array){
@@ -40,5 +47,23 @@ public class Ej7_difBasica {
         }
         
         System.out.println("Mayores son: "+contMay+" , menores son: "+contMen);
+    }
+    
+        public static void porcentajesPeso(Persona[] array){
+        int contBajo = 0;
+        int contIdeal = 0;
+        int contSobre = 0;
+        
+        for (int i = 0; i < array.length; i++){
+            if (array[i].calcularIMC() == -1){
+                contBajo++;
+            } if (array[i].calcularIMC() == 0){
+                contIdeal++;
+            } if (array[i].calcularIMC() == 1){
+                contSobre++;
+            }
+        }
+        
+        System.out.println("Bajo peso son: "+contBajo+" , Ideal son: "+contIdeal+", Sobrepeso son: "+contSobre);
     }
 }
